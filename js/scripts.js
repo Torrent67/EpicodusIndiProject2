5231373b5e4a02a1ba21bdfb07db0bd2e8be79fd
Marqis Ramos <atair01@yahoo.com>
2019-06-07 13:08:38 -0700

    added to js scripts, added to html

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $(".btn").click(function(){
+
+  var q1 = parseInt($("#q1").val());
+  var q2 = parseInt($("#q2").val());
+  var q3 = parseInt($("#q3").val());
+  var q4 = parseInt($("#q4").val());
+  var q5 = parseInt($("#q5").val());
+  var q6 = parseInt($("#q6").val());
+
+  var combine = function(q1, q2, q3, q4, q5, q6) {
+  return q1 + q2 + q3 + q4 + q5 + q6;{
+
+  }
+  var result = combine(q1, q2, q3, q4, q5, q6);
+
+  if(result 
